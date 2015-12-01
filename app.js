c3750eb371ab01e52e13924ba20251a389c15139
MishaFrenkman <m.pomogajko@gmail.com>
2015-11-30 18:37:47 -0800

    Login

--- app.js
+++ app.js
@@ -5,38 +5,37 @@ var options = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.crt')
 };
+var auth = require('http-auth');
+var basic = auth.basic({
+    realm: "No un-authorized access!!",
+    file: __dirname + "/pwd/users.htpasswd" // gevorg:gpass, Sarah:testpass ... 
+});
 
 var server = require('https').createServer(options, app).listen(3000, function(){
     console.log("Https server started on port 3000");
 });
 var	io = require('socket.io').listen(server);
 var	nicknames = [];
+var nickname;
 
 app.use(express.static(__dirname + '/scripts'));
+app.use(auth.connect(basic));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
+    nickname = req.user;
 });
 
 
-io.on('connection', function(socket){
-	socket.on('new user', function(data, callback){
-		if (nicknames.indexOf(data) != -1){
-			callback(false);
-		} else{
-			callback(true);
-			socket.nickname = data;
-			nicknames.push(socket.nickname);
-			updateNicknames();
-		}
-	});
-	
-	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
-	}
+io.on('connection', function(socket){ 
+    socket.on('connected', function(){
+        socket.nickname = nickname
+        nicknames.push(socket.nickname);
+        updateNicknames();      
+    });
 
 	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+		io.emit('new message', {msg: data, nick: socket.nickname});
 	});
 	
 	socket.on('disconnect', function(data){
@@ -44,4 +43,8 @@ io.on('connection', function(socket){
 		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
 		updateNicknames();
 	});
+    
+    function updateNicknames(){
+		io.emit('usernames', nicknames);
+	}
 });
